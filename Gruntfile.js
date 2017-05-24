3eb71ab6a7dfd510661da246ff4f89f076198b8c
Jonas <mail@jonasmohr.de>
2017-05-24 16:14:39 +0200

    technology stack

---
+++ Gruntfile.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = function(grunt)
+{
+
+    grunt.loadNpmTasks("grunt-sass");
+    grunt.loadNpmTasks("grunt-contrib-watch");
+    grunt.loadNpmTasks("grunt-contrib-copy");
+
+    grunt.initConfig({
+        pgk: grunt.file.readJSON("package.json"),
+
+        sass: {
+            options: {
+                sourceMap: true
+            },
+            dist: {
+                files: {
+                    'www/main.css': 'src/sass/main.scss'
+                }
+            }
+        },
+        copy: {
+            for_www: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: "src/",
+                        src: ["js/**", "libs/**", "index.html"],
+                        dest: "www"
+                    }
+
+                ]
+            }
+        },
+        watch: {
+            options: {
+                spawn: false,
+                livereload: true
+            },
+            sass: {
+                files: [
+                    'src/sass/**',
+                    '!src/sass/*.map'
+                ],
+                tasks: ['sass']
+            },
+            others: {
+                files: [
+                    'src/index.html'
+                ],
+                tasks:['copy:for_www']
+            }
+        }
+
+    });
+
+    grunt.registerTask("default", ["sass", "copy:for_www", "watch"]);
+    grunt.registerTask("run", ["sass", "copy:for_www", "watch"]);
+
+};
---
+++ README.md
@@ -0,0 +1,6 @@
+#Instructions
+Install node (download from website)
+
+Install grunt-cli (npm install -g grunt-cli)
+
+Type "npm install"
