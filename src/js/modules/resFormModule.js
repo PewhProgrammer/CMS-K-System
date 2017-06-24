0361693f5d43fa8404a5428c181c7e76fcd8bd23
Marc Schubhan <s8mcschu@stud.uni-saarland.de>
2017-06-24 16:24:10 +0200

    resource overview now as table, table can be sorted, preview slides in

--- src/js/modules/resFormModule.js
+++ src/js/modules/resFormModule.js
@@ -21,20 +21,41 @@
             base.el = el;
             base.$el = jQuery(el);
 
-            /*
+            var selected = 0;
+
             base.$el.find(".k-selectable").each(function () {
-                if ($(this).find("input").is(":checked"))
-                    $(this).css({"border-color": "#333333"});
-            });
-*/
-            base.$el.find(".k-selectable").click(function () {
                 if ($(this).find("input").is(":checked")) {
-                    $(this).css({"border-color": "#333333"});
-                } else {
-                    $(this).css({"border-color": "transparent"});
+                    $(this).parent().parent().css({"background-color": "blanchedalmond"});
+                    selected++;
                 }
             });
 
+            base.$el.find(".k-selectable").find("input").change(function () {
+                if (this.checked) {
+                    $(this).parent().parent().parent().css({"background-color": "blanchedalmond"});
+                    selected++;
+                } else {
+                    $(this).parent().parent().parent().css({"background-color": "transparent"});
+                    selected--;
+                }
+
+                if (selected === 0) {
+                    $("#previewPanel").fadeOut();
+                    $("#resForm").delay(400).animate({width: "100%"});
+                } else if (selected === 1) {
+                    $("#resForm").animate({width: "66%"});
+                    $("#previewPanel").delay(400).fadeIn();
+                    // add details for monitor preview here
+                } else {
+                    // add details for multiple monitor preview here
+                }
+            });
+
+
+            document.getElementById("sortNameDown").onclick = function() {sortTable(0, 'down');};
+            document.getElementById("sortNameUp").onclick = function() {sortTable(0, 'up');};
+            document.getElementById("sortTypeDown").onclick = function() {sortTable(1, 'down');};
+            document.getElementById("sortTypeUp").onclick = function() {sortTable(1, 'up');};
 
             base.$el.find(".fa-trash-o").click(function() {
                 console.log("deleting: " + $(this).data("id"));
@@ -61,3 +82,50 @@
         });
     };
 })(jQuery)
+
+//slightly adjusted exmaple from w3schools
+function sortTable(column, direction) {
+    var table, rows, switching, i, x, y, shouldSwitch;
+    table = document.getElementById("resourceTable");
+    switching = true;
+    /*Make a loop that will continue until
+     no switching has been done:*/
+    while (switching) {
+        //start by saying: no switching is done:
+        switching = false;
+        rows = table.getElementsByTagName("TR");
+        /*Loop through all table rows (except the
+         first, which contains table headers):*/
+        for (i = 1; i < (rows.length - 1); i++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+             one from current row and one from the next:*/
+            x = rows[i].getElementsByTagName("TD")[column];
+            y = rows[i + 1].getElementsByTagName("TD")[column];
+            //check if the two rows should switch place:
+            var xInner, yInner;
+            if(column === 0) {
+                xInner = x.getElementsByTagName("p")[0].innerHTML.toLowerCase();
+                yInner = y.getElementsByTagName("p")[0].innerHTML.toLowerCase();
+            } else {
+                xInner = x.innerHTML.toLowerCase();
+                yInner = y.innerHTML.toLowerCase();
+            }
+            if (direction === "down" && xInner > yInner) {
+                //if so, mark as a switch and break the loop:
+                shouldSwitch= true;
+                break;
+            } else if (direction === "up" && yInner > xInner) {
+                shouldSwitch= true;
+                break;
+            }
+        }
+        if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+             and mark that a switch has been done:*/
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+        }
+    }
+}
