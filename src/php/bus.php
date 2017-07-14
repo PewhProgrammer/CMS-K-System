5622a0a0dc4437ab0ab975aceb96f35cdb784bd0
echatterjee <esha.bt2010@gmail.com>
2017-07-14 20:23:19 +0200

    edited to run python from within mamp

--- src/php/bus.php
+++ src/php/bus.php
@@ -5,6 +5,6 @@
  * Date: 7/14/2017
  * Time: 1:33 PM
  */
-$result=exec("python cache/bus.py");
+$result=exec("MAMP/bin/python/bin/python cache/bus.py");
 $shipments = file_get_contents("cache/bus_1.json", "r");
 echo $shipments;
--- src/php/mensa.php
+++ src/php/mensa.php
@@ -5,6 +5,6 @@
  * Date: 7/14/2017
  * Time: 1:33 PM
  */
-$result=exec("python cache/mensa.py");
+$result=exec("MAMP/bin/python/bin/python cache/mensa.py");
 $shipments = file_get_contents("cache/mensa_1.json", "r");
 echo $shipments;
