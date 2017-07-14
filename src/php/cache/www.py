23be067534570bb9fcffc2af3b4d70e4d7e0328d
echatterjee <esha.bt2010@gmail.com>
2017-07-14 21:20:30 +0200

    edited to run python scripts if required from phpstorm terminal

--- src/php/cache/www.py
+++ src/php/cache/www.py
@@ -1,14 +1,14 @@
-from urllib.request import Request, urlopen
+import urllib2
 from io import TextIOWrapper
 
 def fetch_url(url, encoding = "utf-8"):
     #print(url)
-    req = Request(
+    req = urllib2.Request(
         url = url,
         data = None, 
         headers = {
             "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:51.0) Gecko/20100101 Firefox/51.0"
         }
     )
-    with urlopen(req) as f:
-        return f.read().decode(encoding)
+    response=urllib2.urlopen(req).read().decode(encoding)
+    return response
