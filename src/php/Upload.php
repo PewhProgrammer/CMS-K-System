b03a6fd54d6bdda92d53dfb428dc7f5d41016bbd
Jonas <mail@jonasmohr.de>
2017-07-27 10:50:22 +0200

    prevent upload bugs for other file types

--- src/php/Upload.php
+++ src/php/Upload.php
@@ -56,8 +56,8 @@ class Upload extends Events
 
     protected function verify()
     {
-        if ($this->fileType == 'pdf') $this->resource->setType("pdf");
-        else if ($this->fileType == 'ics') $this->resource->setType("caldav");
+        if (strcasecmp($this->fileType, 'pdf')) $this->resource->setType("pdf");
+        else if (strcasecmp($this->fileType, 'ics')) $this->resource->setType("caldav");
         else if (strcasecmp($this->fileType, 'jpg') == 0 || strcasecmp($this->fileType, 'jpeg') == 0|| strcasecmp($this->fileType, 'png') == 0|| strcasecmp($this->fileType, 'gif') == 0) $this->resource->setType("image");
         else {
             echo "The file type is not supported.";
